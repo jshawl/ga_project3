6f74dd2fd895aad65fcfaa92265e36fc3f497a5e
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-05-11 18:25:59 -0400

    basic functionaility updated

--- app/controllers/brunches_controller.rb
+++ app/controllers/brunches_controller.rb
@@ -5,17 +5,12 @@ class BrunchesController < ApplicationController
 	end
 
 	def search
-
     #@brunch_search = Yelp.client.search('DC', { category_filter: 'breakfast_brunch', limit: 1 })
-
 		# would rather use neighborhood than zipcode here...
 		location = params[:location]
-
 	  parameters = { category_filter: 'breakfast_brunch', limit: 10 }
     @brunch_search = Yelp.client.search( location, parameters )
 		@brunch = @brunch_search.businesses.sample
-		
-    # render json: @brunch
 	end
 
 end
