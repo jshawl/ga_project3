e79e4b67bdba17e4274172d1900494262e666ca8
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-05-13 14:22:51 -0400

    added more parameters to favorites and updated css

--- brunchinator/db/schema.rb
+++ brunchinator/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513174539) do
+ActiveRecord::Schema.define(version: 20150513180827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(version: 20150513174539) do
     t.integer "user_id"
     t.text    "name"
     t.string  "url"
+    t.string  "rating"
+    t.string  "reviews"
+    t.string  "address"
+    t.string  "phone"
   end
 
   create_table "users", force: :cascade do |t|
