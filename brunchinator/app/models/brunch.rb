46fe083a57d7de3e01c15ca6dc4af1b8cf183715
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-05-13 17:30:03 -0400

    updated file structure

--- brunchinator/app/models/brunch.rb
+++
@@ -1,3 +0,0 @@
-class Brunch < ActiveRecord::Base
-
-end
--- brunchinator/db/migrate/20150510175746_create_brunches.rb
+++
@@ -1,8 +0,0 @@
-class CreateBrunches < ActiveRecord::Migration
-  def change
-    create_table :brunches do |t|
-      t.string :name
-    	t.string :address
-    end
-  end
-end
