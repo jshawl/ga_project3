815c5bfba552c775b3ec083edaa486cf122eaa31
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-05-12 20:40:06 -0400

    css and added routes to navigation bar

--- brunchinator/app/controllers/brunches_controller.rb
+++ brunchinator/app/controllers/brunches_controller.rb
@@ -8,7 +8,7 @@ class BrunchesController < ApplicationController
     #@brunch_search = Yelp.client.search('DC', { category_filter: 'breakfast_brunch', limit: 1 })
 		# would rather use neighborhood than zipcode here...
 		location = params[:location]
-	  parameters = { category_filter: 'breakfast_brunch', limit: 10 }
+	  parameters = { category_filter: 'breakfast_brunch', limit: 20 }
     @brunch_search = Yelp.client.search( location, parameters )
 		@brunch = @brunch_search.businesses.sample
 	end
